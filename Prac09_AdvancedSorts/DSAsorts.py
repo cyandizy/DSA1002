630acedf14f62ea3acef7c610693a429375bd0fb
lyns0 <lynzies.sl@gmail.com>
2025-04-28 11:50:05 +0800

    Add: Prac09

---
+++ Prac09_AdvancedSorts/DSAsorts.py
@@ -0,0 +1,119 @@
+#
+# Data Structures and Algorithms COMP1002
+#
+# Python file to hold all sorting methods
+#
+
+import numpy as np
+
+
+def bubbleSort(A):
+    swapped = False
+    for i in range(0, len(A)):
+        for j in range(1, len(A) - i):
+            if A[j] < A[j - 1]:
+                temp = A[j]
+                A[j] = A[j - 1]
+                A[j - 1] = temp
+
+                swapped = True
+
+        if not swapped:
+            break
+
+
+def insertionSort(A):
+    for n in range(1, len(A)):
+        i = n
+
+        while i > 0 and A[i - 1] > A[i]:
+            temp = A[i]
+            A[i] = A[i - 1]
+            A[i - 1] = temp
+
+            i -= 1
+
+def selectionSort(A):
+    for n in range(0, len(A)):
+        minIdx = n
+        for i in range(n + 1, len(A)):
+            if (A[i] < A[minIdx]):
+                minIdx = i
+
+        temp = A[minIdx]
+        A[minIdx] = A[n]
+        A[n] = temp
+
+def mergeSort(A):
+    mergeSortRecurse(A, 0, len(A) - 1)
+
+def mergeSortRecurse(A, leftIdx, rightIdx):
+    if leftIdx < rightIdx:
+        midIdx = (leftIdx + rightIdx) // 2
+
+        mergeSortRecurse(A, leftIdx, midIdx)
+        mergeSortRecurse(A, midIdx + 1, rightIdx)
+
+        merge(A, leftIdx, midIdx, rightIdx)
+
+def merge(A, leftIdx, midIdx, rightIdx):
+    tmpA = np.zeros(rightIdx - leftIdx + 1)
+    i = leftIdx
+    j = midIdx + 1
+    k = 0
+
+    while i <= midIdx and j <= rightIdx:
+        if (A[i] <= A[j]):
+            tmpA[k]= A[i]
+            i += 1
+        else:
+            tmpA[k] = A[j]
+            j += 1
+        
+        k += 1
+
+    for i in range(i, midIdx + 1):
+        tmpA[k] = A[i]
+        k += 1
+    
+    for j in range(j, rightIdx + 1):
+        tmpA[k] = A[j]
+        k += 1
+
+    for k in range(leftIdx, rightIdx + 1):
+        A[k] = tmpA[k - leftIdx]
+
+
+def quickSort(A):
+    """ quickSort - front-end for kick-starting the recursive algorithm
+    """
+    ...
+
+def quickSortRecurse(A, leftIdx, rightIdx):
+    if (right_idx > left_idx):
+        piv_idx = (left_idx + right_idx) // 2
+        new_piv_idx = _partition(arr, left_idx, right_idx, piv_idx)
+
+        _quick_sort(arr, left_idx, new_piv_idx - 1)
+        _quick_sort(arr, new_piv_idx + 1, right_idx)
+
+def doPartitioning(A, leftIdx, rightIdx, pivotIdx):
+    pivValue = arr[pivotIdx]
+    arr[pivotIdx] = arr[rightIdx]
+    arr[rightIdx] = pivValue
+
+    currentIdx = leftIdx
+
+    for i in range(leftIdx, rightIdx):
+        if (arr[i] < pivValue):
+            arr[i], arr[currentIdx] = arr[currentIdx], arr[i]
+            
+            currentIdx += 1
+
+    newPivotIdx = currentIdx
+    arr[rightIdx] = arr[newPivotIdx]
+    arr[newPivotIdx] = pivValue
+
+    return newPivotIdx
+
+
